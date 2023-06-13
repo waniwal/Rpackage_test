75db1925a240702dc9333f85d454e502d11519cb
xiaoxiaolu <luxiaojian1014@qq.com>
2023-06-13 22:42:18 +0800

    my first commit

---
+++ R/miao.R
@@ -0,0 +1,17 @@
+#' Return ties of miao
+#'
+#' @param x
+#'
+#' @return c
+#' @export
+#'
+#' @examples
+#' library(SevenBaby)
+#' miao(1)
+miao = function(x) {
+  a = "miao"
+  c = rep(a,times = x)
+  return(c)
+}
+
+
